8218f9c94a8dbd0cd11ed72f338d46f6a1e051c2
maxiaole <17600402717@163.com>
2017-11-27 15:26:54 +0800

    提交p

--- src/main/java/cn/cocktail/mapper/ProductonsMapper.java
+++ src/main/java/cn/cocktail/mapper/ProductonsMapper.java
@@ -1,10 +1,37 @@
 package cn.cocktail.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+import cn.cocktail.pojo.Productions;
+
 /**
  * 类型接口
  * @author Hulsen
  *
  */
 public interface ProductonsMapper {
+	/**
+	 * 动态查询
+	 * @param map
+	 * @return
+	 */
+List<Productions> findByPage(Map<String, Object> map);
+
+/**
+ * 总条数
+ * @param map
+ * @return
+ */
+Integer countByPage(Map<String, Object>map);
+/**
+ * 通过Id查询全部
+ * @param id
+ * @return
+ */
+Productions findProductions(@Param("id")Integer id);
 
 }
  
--- src/main/java/cn/cocktail/service/ProductonsService.java
+++ src/main/java/cn/cocktail/service/ProductonsService.java
@@ -1,5 +1,25 @@
 package cn.cocktail.service;
 
-public interface ProductonsService {
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
+import cn.cocktail.pojo.Productions;
+import cn.cocktail.tools.Page;
+
+public interface ProductonsService {
+	/**
+	 * 动态查询
+	 * @param map
+	 * @return
+	 */
+Page<Productions> findByPage(Map<String, Object> map);
+
+
+/**
+ * 通过Id查询全部
+ * @param id
+ * @return
+ */
+Productions findProductions(@Param("id")Integer id);
 }
